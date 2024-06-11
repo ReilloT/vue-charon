dc8b901bc3604dd5af50fac65bf25f0a52ae7c65
reillo <17766540024@163.com>
2024-06-12 07:21:48 +0800

    commit

---
+++ src/views/Index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex flex-wrap gap-4">
+    <RouterLink to="/blog">
+        <el-card shadow="never">笔  记</el-card>
+    </RouterLink>
+    <RouterLink to="/blog">
+        <el-card shadow="never">网  盘</el-card>
+    </RouterLink>
+    <RouterLink to="/blog">
+        <el-card shadow="never">待  定</el-card>
+    </RouterLink>
+    <RouterLink to="/blog">
+        <el-card shadow="never">待  定</el-card>
+    </RouterLink>
+    <RouterLink to="/blog">
+        <el-card shadow="never">待  定</el-card>
+    </RouterLink>
+  </div>
+</template>
+<style scoped>
+.el-card{
+    width: 550px;
+    margin-bottom: 10px;
+    opacity: 0.7;
+}
+.el-card:hover{
+    background-color: aqua;
+}
+</style>
